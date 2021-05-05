3669a0c735e0c9842e32693fa3c25e2fed356085
Eugene Goh <gzkgt11@gmail.com>
2021-05-05 09:11:27 +0800

    Imported date & day using moment.js

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import moment from "moment";
 
 function Weather({ weatherData }) {
   return (
     <Card>
       <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
-        <p>Temprature: {weatherData.main.temp}</p>
-        <p>Sunrise: {weatherData.sys.sunrise}</p>
-        <p>Sunset: {weatherData.sys.sunset}</p>
+        <Card.Header className="header">City name: {weatherData.name}</Card.Header>
+
+        <p>Temprature: {weatherData.main.temp} &deg;C</p>
+        <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-MY')}</p>
+        <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-MY')}</p>
         <p>Description: {weatherData.weather[0].description}</p>
+        <p>Air humidity: {weatherData.main.humidity} %</p>
+
+        <p>Day: {moment().format('dddd')}</p>
+        <p>Date: {moment().format('MMM Do YY')}</p>
+        <p>Current Time: {moment().format('LTS')}</p>
       </Card.Content>
     </Card>
   );
