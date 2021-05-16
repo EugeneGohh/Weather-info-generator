a515802b3858648ad5cfdfef25ac39a74ee84d92
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2021-05-16 16:42:39 +0800

    Update WeatherAPI.js

--- src/utils/WeatherAPI.js
+++ src/utils/WeatherAPI.js
@@ -1,5 +1,5 @@
 const REACT_APP_API_URL = "https://api.openweathermap.org/data/2.5";
-const REACT_APP_API_KEY = "8245439854d1f62f690b08f5ab91fb7f";
+const REACT_APP_API_KEY = "YOUR_API_KEY_HERE";
 const REACT_APP_ICON_URL = "https://openweathermap.org/img/w";
 
 export { REACT_APP_API_KEY, REACT_APP_API_URL, REACT_APP_ICON_URL };
